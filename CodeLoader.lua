eafbd0d22abdcafded697f270c6711f31173a837
tevfn <awdawdawdawdaw85@gmail.com>
2025-01-02 21:23:35 +0400

    Create CodeLoader.lua

---
+++ CodeLoader.lua
@@ -0,0 +1,14 @@
+--// [🏂]
+
+return {
+    {"soulninja1000", true}, {"epictrain15", true}, {"roboninja15", true}, {"christmasninja500", true}, 
+    {"zenmaster15k", true}, {"innerpeace5k", true}, {"skyblades10k", true}, {"darkelements2000", true}, 
+    {"silentshadows1000", true}, {"omegasecrets5000", true}, {"ultrasecrets10k", true}, {"elementmaster750", true}, 
+    {"secretcrystal1000", true}, {"skymaster750", true}, {"legends700m", true}, {"dojomasters500", true}, 
+    {"dragonlegend750", true}, {"zenmaster500", true}, {"epicelements500", true}, {"goldninja500", true}, 
+    {"goldupdate500", true}, {"legends500M", true}, {"senseisanta500", true}, {"blizzardninja500", true}, 
+    {"mythicalninja500", true}, {"legendaryninja500", true}, {"shadowninja500", true}, {"legends200M", true}, 
+    {"epicflyingninja500", true}, {"flyingninja500", true}, {"dragonwarrior500", true}, {"swiftblade300", true}, 
+    {"DesertNinja250", true}, {"fastninja100", true}, {"epicninja250", true}, {"masterninja750", true}, 
+    {"sparkninja20", true}, {"soulhunter5", true}
+}
